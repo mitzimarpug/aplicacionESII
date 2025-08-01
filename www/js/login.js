12670e0507e9525555cd661364688f93e43190a1
mitzimarpug <2237000506@utna.edu.mx>
2025-08-01 16:27:19 -0600

    Creación de pantalla incial, login, registro y pantalla principal

---
+++ www/js/login.js
@@ -0,0 +1,16 @@
+function registro(){
+    Swal.fire({
+        title: "Registro exitoso",
+        width: 600,
+        padding: "3em",
+        color: "#000",
+        background: "#fff url(/images/trees.png)",
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+      });
+
+}
